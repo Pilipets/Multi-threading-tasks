af7564b92dbbb087d0e58862c4c7913b7624e046
Pilipets <glebon1802@gmail.com>
2019-09-29 11:23:39 +0200

    Output pipe stream is working

--- Chentsov_Lab1/Project1/Manager.cpp
+++ Chentsov_Lab1/Project1/Manager.cpp
@@ -1,22 +1,28 @@
 #include "Manager.h"
 #include <boost/process.hpp>
 #include <iostream>
+#include <string>
+using namespace std;
 namespace bp = boost::process;
 namespace spos::lab1 {
-	void Manager::Run(int argc, char ** argv)
+	void Manager::Run(int argc, char **argv)
 	{
 		if (argc == 1) {
 			std::cout << "Main process " << std::endl;
-			bp::opstream in;
-			bp::child c("Project1.exe", "OPTIONAL", bp::std_in < in);
-			in << 5 << std::endl;
+
+			bp::ipstream s_out;
+			bp::child c("Project1.exe", "OPTIONAL", bp::std_out > s_out);
+
+			string line;
+			while (c.running() && std::getline(s_out, line) && !line.empty())
+				cout << line << endl;
 
 			c.wait();
 		}
-		else //
+		else
 		{
 			int x = -1;
-			std::cin >> x;
+			//std::cin >> x;
 			std::cout << x << "Separate process" << std::endl;
 		}
 	}
--- Chentsov_Lab1/Project1/Source.cpp
+++ Chentsov_Lab1/Project1/Source.cpp
@@ -3,7 +3,7 @@
 using namespace spos::lab1;
 
 int main(int argc, char **argv) {
-
+	std::cout << argc << std::endl;
 	Manager m;
 	m.Run(argc, argv);
 
